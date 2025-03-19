fa03eede451a8f058704f30ac48416a27e830606
Abhinay Yadav <abhinay01yadav1999@gmail.com>
2025-03-19 21:36:25 +0530

    re traing of model

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,14 +14,3 @@ with open("metrics.txt", "w") as f:
     f.write(f"Accuracy: {accuracy}\n")
 
 print("Model evaluation complete.")
-
-
-import json
-
-metrics = {
-    "accuracy": accuracy,
-    "loss": 1-accuracy
-}
-
-with open("metrics.json", "w") as f:
-    json.dump(metrics, f)
